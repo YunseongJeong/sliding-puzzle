1fe466d54858e5b8d4074a15136348f7b000700a
mandu <jys0972@gmail.com>
2025-01-27 20:40:48 +0900

    feat(PieceMaker.cs) : texture를 받아서 piece들을 만들어주는 util class 구현

---
+++ Assets/Scripts/ImageUtil/PieceMaker.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ImageUtil
+{
+    public class PieceMaker
+    {
+        private Texture _texture;
+        
+        public PieceMaker(Texture texture)
+        {
+            _texture = texture;
+        }
+
+        public GameObject[] GetPieces(int cols, int rows, Transform transform)
+        {
+            GameObject[] pieces = new GameObject[rows * cols];
+            float w = 1f / cols;
+            float h = 1f / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    pieces[i * cols + j] = MakePiece(w, h, j, i, transform);
+                }
+            }
+
+            return pieces;
+        }
+
+        private GameObject MakePiece(float w, float h, int col, int row, Transform transform)
+        {
+            GameObject piece = new GameObject("Piece");
+            piece.transform.SetParent(transform);
+            piece.transform.localScale = Vector3.one;
+            piece.transform.localPosition = Vector3.zero;
+            piece.transform.localRotation = Quaternion.identity;
+            piece.transform.localEulerAngles = Vector3.zero;
+                    
+            piece.AddComponent<CanvasRenderer>();
+            RawImage rawImage = piece.AddComponent<RawImage>();
+            rawImage.texture = _texture;
+            rawImage.uvRect = new Rect(col * w, row * h, w, h);
+            return piece;
+        }
+    }
+}
