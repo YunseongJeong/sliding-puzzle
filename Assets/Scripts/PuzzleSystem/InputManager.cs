63a7122bbe80ca26f745d293038935ee8e283781
mandu <jys0972@gmail.com>
2025-01-27 22:30:00 +0900

    feat(InputManager.cs) : mouse의 위치로 piece 받아오는 로직 구현

---
+++ Assets/Scripts/PuzzleSystem/InputManager.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PuzzleSystem
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private GraphicRaycaster _raycaster;
+        private EventSystem _eventSystem;
+        private MonoBehaviour _context;
+
+        public void Start()
+        {
+            _eventSystem = EventSystem.current;
+        }
+
+        public Piece GetHoveredPiece()
+        {
+            PointerEventData pointerData = new PointerEventData(_eventSystem)
+            {
+                position = Input.mousePosition
+            };
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            _raycaster.Raycast(pointerData, results);
+
+            foreach (var result in results)
+            {
+                Piece piece = result.gameObject.GetComponent<Piece>();
+                if (piece != null)
+                    return piece;
+            }
+
+            return null;
+        }
+        
+    }
+}
