9bfe182396b787676d3cf025a8fff49c9b1c3950
mandu <jys0972@gmail.com>
2025-02-12 17:40:46 +0900

    feat(PuzzleInfoObject.cs) : Puzzle의 정보를 넘기는 객체의 component 구현

---
+++ Assets/Scripts/PuzzleSystem/PuzzleInfo/PuzzleInfoObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PuzzleInfoObject : MonoBehaviour
+{
+    public Texture texture;
+    public int n;
+    private void OnEnable()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public PuzzleInfoObject Create(Texture texture, int n)
+    {
+        GameObject puzzleInfoObject = new GameObject("PuzzleInfoObject");
+        PuzzleInfoObject puzzleInfo = puzzleInfoObject.AddComponent<PuzzleInfoObject>();
+        puzzleInfo.texture = texture;
+        puzzleInfo.n = n;
+
+        return puzzleInfo;
+    }
+}
