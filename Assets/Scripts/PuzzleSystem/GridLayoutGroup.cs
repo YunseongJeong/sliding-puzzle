4e7ff6230c8057538204cf2baffca4b519a587c8
mandu <jys0972@gmail.com>
2025-02-12 18:28:49 +0900

    refact(GridLayoutGroup.cs) : 퍼즐 크기 정보 public으로 수정

--- Assets/Scripts/PuzzleSystem/GridLayoutGroup.cs
+++ Assets/Scripts/PuzzleSystem/GridLayoutGroup.cs
@@ -5,8 +5,8 @@ namespace PuzzleSystem
 {
     public class GridLayoutGroup : MonoBehaviour
     {
-        [SerializeField] private int _cols;
-        [SerializeField] private int _rows;
+        [SerializeField] public int cols;
+        [SerializeField] public int rows;
         
         [SerializeField] private RectTransform _puzzleRect;
 
@@ -16,8 +16,8 @@ namespace PuzzleSystem
         
         public void SetLayout(int cols, int rows)
         {
-            _cols = cols;
-            _rows = rows;
+            this.cols = cols;
+            this.rows = rows;
         }
 
         public void Clear()
@@ -34,29 +34,28 @@ namespace PuzzleSystem
             _pieces = pieces;
             RectTransform rectTransform = GetComponent<RectTransform>();
             
-            w = rectTransform.rect.width / _cols;
-            h = rectTransform.rect.height / _rows;
+            w = rectTransform.rect.width / cols;
+            h = rectTransform.rect.height / rows;
                         
             baseX = rectTransform.localPosition.x - _puzzleRect.rect.width / 2 + w / 2;
             baseY = rectTransform.localPosition.y - _puzzleRect.rect.height / 2 + h / 2;
             
             PositionPieces();
         }
-
         
         public GameObject GetPiece(int index)
         {
             return _pieces[index];
         }
         
-        public GameObject GetPiece(int col, int row)
+        public Piece GetPiece(int col, int row)
         {
-            return _pieces[row * _cols + col];
+            return _pieces[row * cols + col].GetComponent<Piece>();
         }
         
         public int GetIndex(int col, int row)
         {
-            return row * _cols + col;
+            return row * cols + col;
         }
         
         public void SwapPieces(GameObject piece1, GameObject piece2)
@@ -72,12 +71,12 @@ namespace PuzzleSystem
 
         private void PositionPieces()
         {
-            for (int i = 0; i < _rows; i++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < _cols; j++)
+                for (int j = 0; j < cols; j++)
                 {
-                    _pieces[i * _cols + j].GetComponent<RectTransform>().sizeDelta = new Vector2(w, h);
-                    _pieces[i * _cols + j].GetComponent<RectTransform>().localPosition = new Vector3(baseX + j * w, baseY + i * h, 0);
+                    _pieces[i * cols + j].GetComponent<RectTransform>().sizeDelta = new Vector2(w, h);
+                    _pieces[i * cols + j].GetComponent<RectTransform>().localPosition = new Vector3(baseX + j * w, baseY + i * h, 0);
                 }
             }
         }
@@ -85,24 +84,24 @@ namespace PuzzleSystem
         public Piece GetEmptyPiece(Piece piece)
         {
             int index = Array.IndexOf(_pieces, piece.gameObject);
-            int col = index % _cols;
-            int row = index / _cols;
+            int col = index % cols;
+            int row = index / cols;
             
             if (col > 0 && !_pieces[index - 1].activeSelf)
             {
                 return _pieces[index - 1].GetComponent<Piece>();
             }
-            if (col < _cols - 1 && !_pieces[index + 1].activeSelf)
+            if (col < cols - 1 && !_pieces[index + 1].activeSelf)
             {
                 return _pieces[index + 1].GetComponent<Piece>();
             }
-            if (row > 0 && !_pieces[index - _cols].activeSelf)
+            if (row > 0 && !_pieces[index - cols].activeSelf)
             {
-                return _pieces[index - _cols].GetComponent<Piece>();
+                return _pieces[index - cols].GetComponent<Piece>();
             }
-            if (row < _rows - 1 && !_pieces[index + _cols].activeSelf)
+            if (row < rows - 1 && !_pieces[index + cols].activeSelf)
             {
-                return _pieces[index + _cols].GetComponent<Piece>();
+                return _pieces[index + cols].GetComponent<Piece>();
             }
 
             return null;
