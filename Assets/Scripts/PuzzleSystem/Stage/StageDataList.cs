18250483c7baeb3846e690c03b0bbec6f9f6bb6a
mandu <jys0972@gmail.com>
2025-02-12 21:03:14 +0900

    feat(StageDataList.cs) : Stage Data list ScriptableObject 구현

---
+++ Assets/Scripts/PuzzleSystem/Stage/StageDataList.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StageData
+{
+    [SerializeField] public string stageName;
+    [SerializeField] public string stageImagePath;
+    [SerializeField] public int n;
+    [SerializeField] public string clearText;
+    [SerializeField] public string clearImagePath;
+}
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "StageDataList", menuName = "StageDataList")]
+public class StageDataList : ScriptableObject
+{
+    public List<StageData> stageDataList;
+}
